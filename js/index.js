a9b6e6ad3b13ac5ed226f85ab6024362f5082223
Mao-Heng Cheng <mmc007@ucsd.edu>
2018-01-28 17:10:35 -0800

    added some basic js to index and signup so that can navigate some pages more easily now. No real logic behind the page jumping though

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function signUpPress(){
+    window.location.href = "signUp.html";
+}
+function signInPress(){
+    window.location.href = "home.html";
+}
---
+++ js/signUp.js
@@ -0,0 +1,6 @@
+function cancelPress(){
+    window.location.href = "index.html";
+}
+function submitPress(){
+    window.location.href = "index.html";
+}
