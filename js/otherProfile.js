be1937ff6687c906de196ccb9cbf3f8380992826
Mao-Heng Cheng <mmc007@ucsd.edu>
2018-02-04 21:37:13 -0800

    created draft of other user profile html page

---
+++ js/otherProfile.js
@@ -0,0 +1,11 @@
+function logoutPress(){
+    window.location.href = "index.html";
+}
+
+function homePress(){
+    window.location.href = "home.html";
+}
+
+function profilePress() {
+    window.location.href = "myProfile.html";
+}
