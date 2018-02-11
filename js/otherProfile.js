57488d23e2f7ed77afa5c8dc33a7c7f1b5d9f74e
Mao-Heng Cheng <mmc007@ucsd.edu>
2018-02-10 19:22:16 -0800

    added the js to otherProfile

--- js/otherProfile.js
+++ js/otherProfile.js
@@ -11,6 +11,43 @@ var Other ={
     otherGroups : "Village East 1, Village East 2"
   };
 
+
+function startUp(){
+    setOtherInfo();
+};
+  
+  function setOtherInfo(){
+    var otherImg = document.getElementById("otherProfile");
+    otherImg.src = "pix/otherProfile.jpg";
+    otherImg.alt = "pix/random.jpeg";
+
+    var para = document.getElementById("otherPerson");
+    var node = document.createTextNode(Other.otherFirstName + " " 
+    + Other.otherLastName);
+    para.appendChild(node);
+  
+    para = document.getElementById("otherItems");
+    node = document.createTextNode(Other.otherItems);
+    para.appendChild(node);
+  
+    para = document.getElementById("otherEmail");
+    node = document.createTextNode(Other.otherEmail);
+    para.appendChild(node);
+  
+    para = document.getElementById("otherPhone");
+    node = document.createTextNode(Other.otherPhone);
+    para.appendChild(node);
+
+    para = document.getElementById("otherFB");
+    node = document.createTextNode(Other.otherFB);
+    para.appendChild(node);
+  }
+  
+  window.onload = function(){
+    startUp();
+  }
+  
+
 function logoutPress(){
     window.location.href = "index.html";
 }
