7b29b3510875450f0074157d517a131a5184637b
migil <migil@ucsd.edu>
2018-02-15 09:59:30 -0800

    working on js for items and home html

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+
 function homePress(){
     window.location.href = "home.html";
 }
@@ -20,3 +21,48 @@ window.onclick = function(event) {
       }
     }
   }
+
+var groupElem = ["Village East 1", "Village West 2", "Village East 3", "Village West 4"];
+
+
+
+function startUp(){
+  setFeed();
+  setGroups();
+};
+
+function setFeed(){
+  var para = document.getElementById("feed1");
+  var node = document.createTextNode("Justin Gil checked out Wayne Rooney's glue stick");
+  para.appendChild(node);
+
+  para = document.getElementById("feed2");
+  node = document.createTextNode("Leo Qiu checked out Bruno Mars's glue stick");
+  para.appendChild(node);
+
+  para = document.getElementById("feed3");
+  node = document.createTextNode("Derrick Wong checked out Minooc Choo's glue stick");
+  para.appendChild(node);
+
+  para = document.getElementById("feed4");
+  node = document.createTextNode("Scott Chen checked out Jason Hwang's glue stick");
+  para.appendChild(node);
+}
+
+function setGroups(){
+  var para = document.getElementById("groupElem1");
+  var node = document.createTextNode(groupElem[0]);
+  para.appendChild(node);
+
+  para = document.getElementById("groupElem2");
+  node = document.createTextNode(groupElem[1]);
+  para.appendChild(node);
+
+  para = document.getElementById("groupElem3");
+  node = document.createTextNode(groupElem[2]);
+  para.appendChild(node);
+
+  para = document.getElementById("groupElem4");
+  node = document.createTextNode(groupElem[3]);
+  para.appendChild(node);
+}
--- js/items.js
+++ js/items.js
@@ -5,3 +5,64 @@ function profilePress() {
 function logoutPress(){
   window.location.href = "signIn.html";
 }
+
+
+var Me ={
+  myFirstName : "Joe",
+  myLastName : "Schmoe",
+  myPicture : "pix/defaultProfile.jpg",
+  myHome : "home.html",
+  myProfile : "myProfile.html",
+  myEmail : "Rando@email.com",
+  myPhone : "(no)sue-number",
+  myFB : "fb.com/lbj",
+  myItems : "soccer ball tape beer",
+  myGroups : "Village East 1, Village East 2"
+};
+
+
+var members = [
+  "Justin Gil",
+  "Scott Chen",
+  "Michael Cheng",
+  "Derrick Wong"
+]; 
+
+var itemList = [
+	"Tape (Scott)",
+	"Post it (Michael)",
+	"Rice cooker (Brandon)",
+	"HDMI (Chris)",
+	"Screw driver (Justin)",
+	"Vacuum cleaner (Leo)"
+];
+
+
+function startUp(){
+	members.forEach(createMembers);
+	itemList.forEach(createItems);
+};
+
+
+function createMembers(item, index, arr){
+  var para = document.createElement("p");
+  para.className="memberElem";
+  var node = document.createTextNode(item);
+  para.appendChild(node);
+  var getDiv = document.getElementById("membersDiv");
+  getDiv.appendChild(para);
+};
+
+function createItems(item, index, arr){
+  var para = document.createElement("a");
+  para.className="button";
+  para.href="itemComments.html"
+  var node = document.createTextNode(item);
+  para.appendChild(node);
+  var getDiv = document.getElementById("itemList");
+  getDiv.appendChild(para);
+};
+
+window.onload = function(){
+  startUp();
+}
