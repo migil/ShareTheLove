937cbfa35c7ac7875e071918912529a2134b4dc9
migil <migil@ucsd.edu>
2018-02-17 22:16:42 -0800

    userprofile and group classes done

---
+++ js/globalData.js
@@ -0,0 +1,41 @@
+var myItemsList = new Array(
+  "Soccer ball",
+  "Tape",
+  "Beers"
+);
+
+var membersArray = new Array (
+  "Justin Gil",
+  "Scott Chen",
+  "Michael Cheng",
+  "Derrick Wong"
+); 
+
+var itemListArray = new Array (
+	"Tape (Scott)",
+	"Post it (Michael)",
+	"Rice cooker (Brandon)",
+	"HDMI (Chris)",
+	"Screw driver (Justin)",
+	"Vacuum cleaner (Leo)"
+);
+
+var Me ={
+  myFirstName : "Joe",
+  myLastName : "Schmoe",
+  myPicture : "pix/defaultProfile.jpg",
+  myHome : "home.html",
+  myProfile : "myProfile.html",
+  myEmail : "Rando@email.com",
+  myPhone : "(no)sue-number",
+  myFB : "fb.com/lbj",
+  myItems : "soccer ball tape beer",
+  myGroups : "Village East 1, Village East 2",
+  fullName : "Joe Schmoe"
+};
+
+
+var groups = [
+  "Village East 1",
+  "Village East 2"
+]
--- js/home.js
+++ js/home.js
@@ -3,7 +3,67 @@
    ~~~~~~~~~~~~~~~~~~~~~~~~ Data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
-var Me ={
+class UserProfile {
+  constructor(firstName, lastName, picture, email, phoneNumber, facebook, myItemList, myGroupList, feedList) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.picture = picture;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.facebook = facebook;
+    this.myItemList = myItemList;
+    this.myGroupList = myGroupList;
+    this.feedList = feedList;
+    this.fullName = firstName + " " + lastName;
+  }
+}
+
+class Group{
+  constructor(memberList, groupItemList){
+    this.memberList = memberList;
+    this.groupItemList = groupItemList;
+  }
+}
+
+//each user materials
+var tempItemList = new Array ("Tape, Soccer Ball, Nuclear missle");
+var tempGroupList = new Array ("Village East 1", "Village East 2");
+var tempFeedList  = new Array (
+  "Justin Gil checked out Wayne Rooney's glue stick",
+  "Leo Qiu checked out Bruno Mars's glue stick",
+  "Derrick Wong checked out Minooc Choo's glue stick",
+  "Scott Chen checked out Jason Hwang's glue stick",
+  "Michael Cheng checked out Issac's macbook charger",
+);
+
+//each group materials
+var tempMemberList = new Array("Justin Gil", "Scott Chen", "Michael Cheng", "Derrick Wong");
+var tempGroupItemList = new Array("Tape", "Post it", "Rice cooker", "HDMI", "Screw driver","Vacuum cleaner");
+
+var tempMemberList2 = new Array("Derrick Wong", "Scott Chen", "Michael Cheng", "Derrick Wong");
+var tempGroupItemList2 = new Array("Tape", "Post it", "iPhone X", "desktop", "Screw driver","Vacuum cleaner");
+
+//UserProfiles
+var Me = new UserProfile("Joe", "Schmoe", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Derrick = new UserProfile("Derrick", "Wong", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Jason = new UserProfile("Jason", "Jiajun", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Leo = new UserProfile("Leo", "Qiu", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Scott = new UserProfile("Scott", "Chen", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+
+//GroupProfile
+var VilEast1 = new Group (tempMemberList, tempGroupItemList);
+var VilWast2 = new Group (tempMemberList2, tempGroupItemList2);
+
+var groupList = new Array(
+  "Village East 1",
+  "Village West 3",
+  "Warren Reshall 4",
+  "UCSD Tennis Group",
+  "UCSD CSE Nerds",
+  "Anime Group"
+);
+
+var Me = {
   myFirstName : "Joe",
   myLastName : "Schmoe",
   myPicture : "pix/defaultProfile.jpg",
@@ -24,15 +84,6 @@ var Me ={
     "Village East 2"
   )
 };
-
-var groupList = new Array(
-  "Village East 1",
-  "Village West 3",
-  "Warren Reshall 4",
-  "UCSD Tennis Group",
-  "UCSD CSE Nerds",
-  "Anime Group"
-);
 
 var feedList = new Array(
   "Justin Gil checked out Wayne Rooney's glue stick",
@@ -77,8 +128,6 @@ var comments = [
   "Joe: me borrow tape",
   "Nico: Hai Hai"
 ];
-
-
 
 
 /*
