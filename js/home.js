ccd872bed1e7e4046df9b834571a9307ba632317
Scott Chen <scottchen625@gmail.com>
2018-02-17 16:15:17 -0800

    Convert our app into a single-page app. Now all html are in home.html, and all js are in home.js (except for signup/signin). Pages switch a lot smoother. State is preserved now (i.e., add an item on myProfile, switch to other pages, the item still persist under myProfile).

--- js/home.js
+++ js/home.js
@@ -1,28 +1,7 @@
 
-function homePress(){
-    window.location.href = "home.html";
-}
-
-function profilePress() {
-    window.location.href = "myProfile.html";
-}
-
-
-window.onclick = function(event) {
-    if (!event.target.matches('.profileImageClass')) {
-
-      var dropdowns = document.getElementsByClassName("dropDownContent");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
-var groupElem = ["Village East 1", "Village West 2", "Village East 3", "Village West 4"];
+/*
+   ~~~~~~~~~~~~~~~~~~~~~~~~ Data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
 var groupList = [
   "Village East 1",
   "Village West 3",
@@ -40,11 +19,112 @@ var feedList = [
   "Michael Cheng checked out Issac's macbook charger",
 ];
 
-function startUp(){
-  groupList.forEach(createGroups);
-  feedList.forEach(createFeeds);
+var Me ={
+  myFirstName : "Joe",
+  myLastName : "Schmoe",
+  myPicture : "pix/defaultProfile.jpg",
+  myHome : "home.html",
+  myProfile : "myProfile.html",
+  myEmail : "Rando@email.com",
+  myPhone : "(no)sue-number",
+  myFB : "fb.com/lbj",
+  myItems : "soccer ball, tape, beer",
+  myGroups : "Village East 1, Village East 2"
 };
 
+var items = [
+  "soccer ball",
+  "tape",
+  "beer"
+]
+
+var groups = [
+  "Village East 1",
+  "Village East 2"
+]
+
+var Me ={
+  myFirstName : "Joe",
+  myLastName : "Schmoe",
+  myPicture : "pix/defaultProfile.jpg",
+  myHome : "home.html",
+  myProfile : "myProfile.html",
+  myEmail : "Rando@email.com",
+  myPhone : "(no)sue-number",
+  myFB : "fb.com/lbj",
+  myItems : "soccer ball tape beer",
+  myGroups : "Village East 1, Village East 2",
+  fullName : "Joe Schmoe"
+};
+
+var myItemsList = new Array(
+  "Soccer ball",
+  "Tape",
+  "Beers"
+);
+
+var membersArray = new Array (
+  "Justin Gil",
+  "Scott Chen",
+  "Michael Cheng",
+  "Derrick Wong"
+);
+
+var itemListArray = new Array (
+  "Tape (Scott)",
+  "Post it (Michael)",
+  "Rice cooker (Brandon)",
+  "HDMI (Chris)",
+  "Screw driver (Justin)",
+  "Vacuum cleaner (Leo)"
+);
+
+var Other ={
+  otherFirstName : "Nico",
+  otherLastName : "Nico",
+  otherPicture : "pix/otherProfile.jpg",
+  otherHome : "home.html",
+  otherProfile : "otherProfile.html",
+  otherEmail : "Rando@email.com",
+  otherPhone : "(no)sue-number",
+  otherFB : "fb.com/lbj",
+  otherItems : "soccer ball tape beer",
+  otherGroups : "Village East 1, Village East 2"
+};
+
+var comments = [
+  "Joe: Hello",
+  "Nico: konichiwa",
+  "Joe: me borrow tape",
+  "Nico: Hai Hai"
+];
+
+
+
+
+/*
+   ~~~~~~~~~~~~~~~~ ENTRY POINT: for loading everything ~~~~~~~~~~~~~~~~
+*/
+window.onload = function() {
+  startUpHomePage();
+  startUpMyProfilePage();
+  startUpItemsPage();
+  startUpItemCommentsPage();
+}
+
+
+// *********************************************************************************
+//***************************  Home.html   *****************************************
+// *********************************************************************************
+
+function startUpHomePage() {
+  groupList.forEach(createGroups);
+  feedList.forEach(createFeeds);
+}
+
+/*
+   ~~~~~~~~~~~~~~~~ Loads Groups & Comment Feed for home.html ~~~~~~~~~~~~~~~~
+*/
 function createFeeds(item, index, arr){
   var para = document.createElement("p");
   para.className ="activityElem";
@@ -57,9 +137,382 @@ function createFeeds(item, index, arr){
 function createGroups(item, index, arr){
   var para = document.createElement("a");
   para.className="button";
-  para.href="items.html"
+  para.onclick = function() {
+    groupPress();
+  }
   var node = document.createTextNode(item);
   para.appendChild(node);
   var getDiv = document.getElementById("groupList");
   getDiv.appendChild(para);
 };
+
+
+/*
+   ~~~~~~~~~~~~~ Navigating to different pages ~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+function homePress(){
+  console.log("to home page!");
+  document.getElementById("home_page").style.display = "inline";
+
+  // hide all other pages
+  document.getElementById("myProfile_page").style.display = "none";
+  document.getElementById("items_page").style.display = "none";
+  document.getElementById("itemComments_page").style.display = "none";
+}
+
+function profilePress() {
+  console.log("to profile page!");
+  document.getElementById("myProfile_page").style.display = "inline";
+
+  // hide all other pages
+  document.getElementById("home_page").style.display = "none";
+  document.getElementById("items_page").style.display = "none";
+  document.getElementById("itemComments_page").style.display = "none";
+}
+
+function groupPress() {
+  console.log("to group page!");
+  document.getElementById("items_page").style.display = "inline";
+
+  // hide all other pages
+  document.getElementById("home_page").style.display = "none";
+  document.getElementById("myProfile_page").style.display = "none";
+  document.getElementById("itemComments_page").style.display = "none";
+}
+
+function itemPress() {
+  console.log("to item comments page!");
+  document.getElementById("itemComments_page").style.display = "inline";
+
+  // hide all other pages
+  document.getElementById("home_page").style.display = "none";
+  document.getElementById("myProfile_page").style.display = "none";
+  document.getElementById("items_page").style.display = "none";
+}
+
+function logoutPress() {
+  window.location.href = "signIn.html";
+};
+
+
+/*
+  ~~~~~~~~~~~~~~~~~~~~~~~ Dropdown Menu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+window.onclick = function(event) {
+  if (!event.target.matches('.profileImageClass')) {
+
+    var dropdowns = document.getElementsByClassName("dropDownContent");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+// *********************************************************************************
+//***************************  myProfile.html   ************************************
+// *********************************************************************************
+
+/*
+   ~~~~~~~~~~~~~~~~ Loads profile info for myProfile.html ~~~~~~~~~~~~~~~~
+*/
+function startUpMyProfilePage() {
+  items.forEach(autoCreateItems);
+  groups.forEach(autoCreateGroups);
+  setOtherInfoMyProfilePage();
+
+  var myImg = document.getElementById("profileImage");
+  myImg.src = "pix/defaultProfile.jpg";
+  myImg.alt = "pix/random.jpeg";
+};
+
+var counter= 1;
+function autoCreateItems(item, index, arr) {
+  var para = document.createElement("button");
+  para.className="contactInfoItem";
+
+  para.id = "item_button_" + counter;
+  //para.onclick = toggle(para.id);
+  para.onclick = function() {
+
+    if (para.style.backgroundColor == "rgb(234, 231, 220)" || para.style.backgroundColor == "") {
+      para.style.backgroundColor = "red";
+    } else {
+      para.style.backgroundColor = "rgb(234, 231, 220)";
+    }
+
+    console.log(para.style.backgroundColor);
+    console.log(para.style.color);
+  }
+  counter += 1;
+
+  var node = document.createTextNode(item);
+  para.appendChild(node);
+
+  var getDiv = document.getElementById("itemSection");
+  getDiv.appendChild(para);
+}
+
+function autoCreateGroups(item, index, arr) {
+  // create p tag
+  var para = document.createElement("p");
+  // add class to p tag
+  para.className = "contactInfoGroup";
+  // add text to the tag
+  var node = document.createTextNode(item);
+  para.appendChild(node);
+
+  var myDiv = document.getElementById("profileGroups");
+  myDiv.appendChild(para);
+}
+
+function setOtherInfoMyProfilePage() {
+  var otherImg = document.getElementById("profilePic");
+  otherImg.src = "pix/defaultProfile.jpg";
+  otherImg.alt = "pix/random.jpeg";
+
+  var para = document.getElementById("profileName");
+  var node = document.createTextNode(Me.myFirstName + " " + Me.myLastName);
+  para.appendChild(node);
+
+  para = document.getElementById("profileEmail");
+  node = document.createTextNode(Me.myEmail);
+  para.appendChild(node);
+
+  para = document.getElementById("profilePhone");
+  node = document.createTextNode(Me.myPhone);
+  para.appendChild(node);
+
+  para = document.getElementById("profileFacebook");
+  node = document.createTextNode(Me.myFB);
+  para.appendChild(node);
+}
+
+/*
+   ~~~~~~~~~~~~~~~~ Add new item to myProfile.html! ~~~~~~~~~~~~~~~~
+*/
+function addItem() {
+  var addItemButton = document.getElementById("addItemButton");
+  addItemButton.style.display = "none";
+
+  var submitItemButton = document.getElementById("submitItemButton");
+  submitItemButton.style.display = "inline";
+
+  var newItemText = document.getElementById("newItemText");
+  newItemText.style.display = "inline";
+}
+
+function submitItem() {
+  var submitItemButton = document.getElementById("submitItemButton");
+  submitItemButton.style.display = "none";
+
+  var newItemText = document.getElementById("newItemText");
+  newItemText.style.display = "none";
+
+  var addItemButton = document.getElementById("addItemButton");
+  addItemButton.style.display = "inline";
+
+  var newItem = document.getElementById("newItemText").value;
+  var node = document.createTextNode(newItem);
+
+  var para = document.createElement("button");
+  para.className="contactInfoItem";
+  para.id = "item_button_" + counter;
+  counter += 1;
+
+  para.appendChild(node);
+
+  var getDiv = document.getElementById("itemSection");
+  getDiv.appendChild(para);
+}
+
+
+
+// *********************************************************************************
+//******************************  items.html   *************************************
+// *********************************************************************************
+var memberDiv;
+var itemDiv;
+var memList;
+var itemListByClass;
+
+function startUpItemsPage(){
+  memList = document.getElementsByClassName("memberElem");
+  memberDiv = document.getElementById("membersDiv");
+  itemDiv = document.getElementById("itemDiv");
+  itemFetchList = document.getElementsByClassName("button items");
+
+  membersArray.forEach(createMembers);
+  itemListArray.forEach(createItems);
+  displayCurrentGroup();
+};
+
+function createMembers(item, index, arr) {
+  var para = document.createElement("p");
+  para.className="memberElem";
+  var node = document.createTextNode(item);
+
+  if(item == Me.fullName){
+    para.style.color = "#E85A4F";
+  }
+
+  para.appendChild(node);
+  memberDiv.appendChild(para);
+};
+
+function createItems(item, index, arr) {
+  var para = document.createElement("a");
+  para.className="button items";
+  para.onclick = function() {
+    itemPress();
+  }
+  para.id="itemElemId";
+
+  var myItem = false;
+  if(myItemsList.indexOf(item) > -1){
+    para.style.color = "ORANGE";
+    myItem = true;
+    item = item + " (my item)";
+  }
+
+  var node = document.createTextNode(item);
+
+  para.appendChild(node);
+  itemDiv.appendChild(para);
+};
+
+function displayCurrentGroup() {
+  var para = document.createElement("p");
+  para.className = "columnTitle";
+  var node = document.createTextNode("Village East 1");
+  para.appendChild(node);
+  var getDiv = document.getElementById("groupTitle");
+  getDiv.appendChild(para);
+}
+
+
+function joinToggle(checkbox) {
+    //toggle on
+    if (!checkbox.checked){
+
+        for(var i = memList.length - 1; 0 <= i; i--){
+          if(memList[i] && memList[i].parentElement)
+            memList[i].parentElement.removeChild(memList[i]);
+        }
+
+        membersArray.push(Me.fullName);
+        membersArray.forEach(createMembers);
+
+        for(var i = itemFetchList.length - 1; 0 <= i; i--){
+          if(itemFetchList[i] && itemFetchList[i].parentElement)
+            itemFetchList[i].parentElement.removeChild(itemFetchList[i]);
+        }
+
+        for(var i = 0; i < myItemsList.length; i++){
+          var myItemToAdd = myItemsList[i];
+          itemListArray.push(myItemToAdd);
+        }
+        itemListArray.forEach(createItems);
+
+    }
+    //toggle off
+    else{
+      //clear the memList
+        for(var i = memList.length - 1; 0 <= i; i--){
+          if(memList[i] && memList[i].parentElement)
+            memList[i].parentElement.removeChild(memList[i]);
+        }
+
+        //cut off the my name from the meemberArray
+        membersArray.splice(membersArray.indexOf(Me.fullName), 1);
+        membersArray.forEach(createMembers);
+
+
+        //clear all thes item from the displayList
+        for(var i = itemFetchList.length - 1; 0 <= i; i--){
+          if(itemFetchList[i] && itemFetchList[i].parentElement)
+            itemFetchList[i].parentElement.removeChild(itemFetchList[i]);
+        }
+        //remove my items from itemListArray
+        for(var i = 0; i < myItemsList.length; i++){
+          itemListArray.splice(itemListArray.indexOf(myItemsList[i]), 1);
+        }
+        itemListArray.forEach(createItems);
+    }
+}
+
+
+// *********************************************************************************
+//******************************  itemComments.html   ******************************
+// *********************************************************************************
+function startUpItemCommentsPage() {
+  comments.forEach(autoCreateComment);
+  setOtherInfoItemCommentsPage();
+
+  var myImg = document.getElementById("profileImage");
+  myImg.src = "pix/defaultProfile.jpg";
+  myImg.alt = "pix/random.jpeg";
+}
+
+function setOtherInfoItemCommentsPage() {
+  var otherImg = document.getElementById("otherProfile");
+  otherImg.src = "pix/otherProfile.jpg";
+  otherImg.alt = "pix/random.jpeg";
+
+  var para = document.getElementById("otherItem");
+  var node = document.createTextNode("Tape");
+  para.appendChild(node);
+
+  para = document.getElementById("otherOwner");
+  node = document.createTextNode(Other.otherFirstName);
+  para.appendChild(node);
+
+  para = document.getElementById("otherEmail");
+  node = document.createTextNode(Other.otherEmail);
+  para.appendChild(node);
+
+  para = document.getElementById("otherPhone");
+  node = document.createTextNode(Other.otherPhone);
+  para.appendChild(node);
+}
+
+function autoCreateComment(item, index, arr) {
+  var para = document.createElement("p");
+  para.className="comments";
+  var node = document.createTextNode(item);
+  para.appendChild(node);
+
+  var getDiv = document.getElementById("commentsSection");
+  getDiv.appendChild(para);
+};
+
+
+function submitCommentPress() {
+  var comment = document.getElementById("commentText").value;
+
+  var para = document.createElement("p");
+  para.className="comments";
+
+  var node = document.createTextNode(Me.myFirstName + ": " + comment);
+  para.appendChild(node);
+
+  var getDiv = document.getElementById("commentsSection");
+  getDiv.appendChild(para);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
