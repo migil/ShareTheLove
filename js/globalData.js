4d36e0e205116f0e0ce4d7977b7d5c3b301fc0e6
migil <migil@ucsd.edu>
2018-02-18 00:35:01 -0800

    added items appears on the group item list

--- js/globalData.js
+++ js/globalData.js
@@ -1,41 +1,85 @@
-var myItemsList = new Array(
-  "Soccer ball",
-  "Tape",
-  "Beers"
+
+class UserProfile {
+  constructor(firstName, lastName, picture, email, phoneNumber, facebook, myItemList, myGroupList, feedList) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.picture = picture;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.facebook = facebook;
+    this.myItemList = myItemList;
+    this.myGroupList = myGroupList;
+    this.feedList = feedList;
+    this.fullName = firstName + " " + lastName;
+  }
+}
+
+class Group{
+  constructor(memberList, groupItemList){
+    this.memberList = memberList;
+    this.groupItemList = groupItemList;
+  }
+}
+
+//each user materials
+var tempItemList = new Array ("Booze", "Soccer Ball", "Nuclear missle");
+var tempGroupList = new Array ();
+var tempFeedList  = new Array (
+  "Justin Gil checked out Wayne Rooney's glue stick",
+  "Leo Qiu checked out Bruno Mars's glue stick",
+  "Derrick Wong checked out Minooc Choo's glue stick",
+  "Scott Chen checked out Jason Hwang's glue stick",
+  "Michael Cheng checked out Issac's macbook charger",
 );
 
-var membersArray = new Array (
-  "Justin Gil",
-  "Scott Chen",
-  "Michael Cheng",
-  "Derrick Wong"
-); 
+//each group materials
+var tempMemberList = new Array("Justin Gil", "Scott Chen", "Michael Cheng", "Derrick Wong");
+var tempGroupItemList = new Array("Tape", "Post it", "Rice cooker", "HDMI", "Screw driver","Vacuum cleaner");
 
-var itemListArray = new Array (
-	"Tape (Scott)",
-	"Post it (Michael)",
-	"Rice cooker (Brandon)",
-	"HDMI (Chris)",
-	"Screw driver (Justin)",
-	"Vacuum cleaner (Leo)"
+var tempMemberList2 = new Array("Derrick Wong", "Scott Chen", "Michael Cheng", "Derrick Wong");
+var tempGroupItemList2 = new Array("Tape", "Post it", "iPhone X", "desktop", "Screw driver","Vacuum cleaner");
+
+//UserProfiles
+var Me = new UserProfile("Joe", "Schmoe", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Derrick = new UserProfile("Derrick", "Wong", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Jason = new UserProfile("Jason", "Jiajun", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Leo = new UserProfile("Leo", "Qiu", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+var Scott = new UserProfile("Scott", "Chen", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
+
+//GroupProfile
+var VilEast1 = new Group (tempMemberList, tempGroupItemList);
+var VilWest3 = new Group (tempMemberList2, tempGroupItemList2);
+
+var groupList =  Array(
+  "Village East 1",
+  "Village West 3",
 );
 
-var Me ={
-  myFirstName : "Joe",
-  myLastName : "Schmoe",
-  myPicture : "pix/defaultProfile.jpg",
-  myHome : "home.html",
-  myProfile : "myProfile.html",
-  myEmail : "Rando@email.com",
-  myPhone : "(no)sue-number",
-  myFB : "fb.com/lbj",
-  myItems : "soccer ball tape beer",
-  myGroups : "Village East 1, Village East 2",
-  fullName : "Joe Schmoe"
+var groupMap = {
+  "Village East 1" : VilEast1,
+  "Village West 3" : VilWest3 
 };
 
 
-var groups = [
-  "Village East 1",
-  "Village East 2"
-]
+//Michael? 
+var Other ={
+  otherFirstName : "Nico",
+  otherLastName : "Nico",
+  otherPicture : "pix/otherProfile.jpg",
+  otherHome : "home.html",
+  otherProfile : "otherProfile.html",
+  otherEmail : "Rando@email.com",
+  otherPhone : "(no)sue-number",
+  otherFB : "fb.com/lbj",
+  otherItems : "soccer ball tape beer",
+  otherGroups : "Village East 1, Village East 2"
+};
+
+var comments = [
+  "Joe: Hello",
+  "Nico: konichiwa",
+  "Joe: me borrow tape",
+  "Nico: Hai Hai"
+];
+
+
--- js/home.js
+++ js/home.js
@@ -1,135 +1,3 @@
-
-/*
-   ~~~~~~~~~~~~~~~~~~~~~~~~ Data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
-
-class UserProfile {
-  constructor(firstName, lastName, picture, email, phoneNumber, facebook, myItemList, myGroupList, feedList) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.picture = picture;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.facebook = facebook;
-    this.myItemList = myItemList;
-    this.myGroupList = myGroupList;
-    this.feedList = feedList;
-    this.fullName = firstName + " " + lastName;
-  }
-}
-
-class Group{
-  constructor(memberList, groupItemList){
-    this.memberList = memberList;
-    this.groupItemList = groupItemList;
-  }
-}
-
-//each user materials
-var tempItemList = new Array ("Tape, Soccer Ball, Nuclear missle");
-var tempGroupList = new Array ("Village East 1", "Village East 2");
-var tempFeedList  = new Array (
-  "Justin Gil checked out Wayne Rooney's glue stick",
-  "Leo Qiu checked out Bruno Mars's glue stick",
-  "Derrick Wong checked out Minooc Choo's glue stick",
-  "Scott Chen checked out Jason Hwang's glue stick",
-  "Michael Cheng checked out Issac's macbook charger",
-);
-
-//each group materials
-var tempMemberList = new Array("Justin Gil", "Scott Chen", "Michael Cheng", "Derrick Wong");
-var tempGroupItemList = new Array("Tape", "Post it", "Rice cooker", "HDMI", "Screw driver","Vacuum cleaner");
-
-var tempMemberList2 = new Array("Derrick Wong", "Scott Chen", "Michael Cheng", "Derrick Wong");
-var tempGroupItemList2 = new Array("Tape", "Post it", "iPhone X", "desktop", "Screw driver","Vacuum cleaner");
-
-//UserProfiles
-var Me = new UserProfile("Joe", "Schmoe", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
-var Derrick = new UserProfile("Derrick", "Wong", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
-var Jason = new UserProfile("Jason", "Jiajun", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
-var Leo = new UserProfile("Leo", "Qiu", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
-var Scott = new UserProfile("Scott", "Chen", "pix/defaultProfile.jpg", "Rando@email.com", "(393)992-3142", "fb.com/fjnf", tempItemList, tempGroupList, tempFeedList);
-
-//GroupProfile
-var VilEast1 = new Group (tempMemberList, tempGroupItemList);
-var VilWast2 = new Group (tempMemberList2, tempGroupItemList2);
-
-var groupList = new Array(
-  "Village East 1",
-  "Village West 3",
-  "Warren Reshall 4",
-  "UCSD Tennis Group",
-  "UCSD CSE Nerds",
-  "Anime Group"
-);
-
-var Me = {
-  myFirstName : "Joe",
-  myLastName : "Schmoe",
-  myPicture : "pix/defaultProfile.jpg",
-  myHome : "home.html",
-  myProfile : "myProfile.html",
-  myEmail : "Rando@email.com",
-  myPhone : "(no)sue-number",
-  myFB : "fb.com/lbj",
-  myItems : "soccer ball tape beer",
-  fullName : "Joe Schmoe",
-  myItemsList : new Array(
-    "Soccer ball",
-    "Tape",
-    "Beers"
-  ),
-  myGroupList : new Array(
-    "Village East 1",
-    "Village East 2"
-  )
-};
-
-var feedList = new Array(
-  "Justin Gil checked out Wayne Rooney's glue stick",
-  "Leo Qiu checked out Bruno Mars's glue stick",
-  "Derrick Wong checked out Minooc Choo's glue stick",
-  "Scott Chen checked out Jason Hwang's glue stick",
-  "Michael Cheng checked out Issac's macbook charger",
-);
-
-var membersArray = new Array (
-  "Justin Gil",
-  "Scott Chen",
-  "Michael Cheng",
-  "Derrick Wong"
-);
-
-var itemListArray = new Array (
-  "Tape (Scott)",
-  "Post it (Michael)",
-  "Rice cooker (Brandon)",
-  "HDMI (Chris)",
-  "Screw driver (Justin)",
-  "Vacuum cleaner (Leo)"
-);
-
-var Other ={
-  otherFirstName : "Nico",
-  otherLastName : "Nico",
-  otherPicture : "pix/otherProfile.jpg",
-  otherHome : "home.html",
-  otherProfile : "otherProfile.html",
-  otherEmail : "Rando@email.com",
-  otherPhone : "(no)sue-number",
-  otherFB : "fb.com/lbj",
-  otherItems : "soccer ball tape beer",
-  otherGroups : "Village East 1, Village East 2"
-};
-
-var comments = [
-  "Joe: Hello",
-  "Nico: konichiwa",
-  "Joe: me borrow tape",
-  "Nico: Hai Hai"
-];
-
-
 /*
    ~~~~~~~~~~~~~~~~ ENTRY POINT: for loading everything ~~~~~~~~~~~~~~~~
 */
@@ -147,7 +15,7 @@ window.onload = function() {
 
 function startUpHomePage() {
   groupList.forEach(createGroups);
-  feedList.forEach(createFeeds);
+  Me.feedList.forEach(createFeeds);
 }
 
 /*
@@ -176,7 +44,7 @@ function createGroups(item, index, arr){
 
 
 /*
-   ~~~~~~~~~~~~~ Navigating to different pages ~~~~~~~~~~~~~~~~~~~~~~~~
+   ~~~~~~~~~~~~~ Routing: Navigating to different pages ~~~~~~~~~~~~~~~~~~~~~~~~
 */
 function homePress(){
   console.log("to home page!");
@@ -201,6 +69,8 @@ function profilePress() {
 function groupPress() {
   console.log("to group page!");
   document.getElementById("items_page").style.display = "inline";
+  console.log(Me.myItemList);
+  startUpItemsPage();
 
   // hide all other pages
   document.getElementById("home_page").style.display = "none";
@@ -249,7 +119,9 @@ window.onclick = function(event) {
    ~~~~~~~~~~~~~~~~ Loads profile info for myProfile.html ~~~~~~~~~~~~~~~~
 */
 function startUpMyProfilePage() {
-  Me.myItemsList.forEach(autoCreateItems);
+  console.log(Me.myItemList);
+
+  Me.myItemList.forEach(autoCreateItems);
   Me.myGroupList.forEach(autoCreateGroups);
   setOtherInfoMyProfilePage();
 
@@ -260,6 +132,7 @@ function startUpMyProfilePage() {
 
 var counter= 1;
 function autoCreateItems(item, index, arr) {
+  console.log(item);
   var para = document.createElement("button");
   para.className="contactInfoItem";
 
@@ -296,19 +169,19 @@ function setOtherInfoMyProfilePage() {
   otherImg.alt = "pix/random.jpeg";
 
   var para = document.getElementById("profileName");
-  var node = document.createTextNode(Me.myFirstName + " " + Me.myLastName);
+  var node = document.createTextNode(Me.firstName + " " + Me.lastName);
   para.appendChild(node);
 
   para = document.getElementById("profileEmail");
-  node = document.createTextNode(Me.myEmail);
+  node = document.createTextNode(Me.email);
   para.appendChild(node);
 
   para = document.getElementById("profilePhone");
-  node = document.createTextNode(Me.myPhone);
+  node = document.createTextNode(Me.phone);
   para.appendChild(node);
 
   para = document.getElementById("profileFacebook");
-  node = document.createTextNode(Me.myFB);
+  node = document.createTextNode(Me.facebook);
   para.appendChild(node);
 }
 
@@ -352,6 +225,20 @@ function submitItem() {
 
   var getDiv = document.getElementById("itemSection");
   getDiv.appendChild(para);
+
+  // update Me's list of items
+  Me.myItemList.push(newItem);
+
+  console.log(Me.myGroupList);
+
+  // for each group Me is in, their item list must be updated as well
+  for (var i = 0; i < Me.myGroupList.length; i++) {
+    // grab the freaking group object
+    groupMap[Me.myGroupList[i]].groupItemList.push(newItem);
+    console.log(Me.myGroupList[i]);
+  }
+
+  console.log(Me.myItemList);
 }
 
 function itemButtonColorToggle(elem, id) {
@@ -372,18 +259,36 @@ function itemButtonColorToggle(elem, id) {
 var memberDiv;
 var itemDiv;
 var memList;
-var itemListByClass;
+var itemFetchList;
+var groupNameTitle;
+var titleItem;
 
 function startUpItemsPage(){
   memList = document.getElementsByClassName("memberElem");
   memberDiv = document.getElementById("membersDiv");
   itemDiv = document.getElementById("itemDiv");
   itemFetchList = document.getElementsByClassName("button items");
+  titleItem = document.getElementById("groupTitle");
 
-  membersArray.forEach(createMembers);
-  itemListArray.forEach(createItems);
+  //delete item list
+  for(var i = itemFetchList.length - 1; 0 <= i; i--){
+    if(itemFetchList[i] && itemFetchList[i].parentElement)
+      itemFetchList[i].parentElement.removeChild(itemFetchList[i]);
+  }
+  //delte member list
+  for(var i = memList.length - 1; 0 <= i; i--){
+    if(memList[i] && memList[i].parentElement)
+      memList[i].parentElement.removeChild(memList[i]);
+  }
+
+  VilEast1.memberList.forEach(createMembers);
+  VilEast1.groupItemList.forEach(createItems);
+
+  console.log(VilEast1.groupItemList);
+
   displayCurrentGroup();
 };
+
 
 function createMembers(item, index, arr) {
   var para = document.createElement("p");
@@ -398,6 +303,8 @@ function createMembers(item, index, arr) {
   memberDiv.appendChild(para);
 };
 
+
+//DEBUG JUSTIN PLZ
 function createItems(item, index, arr) {
   var para = document.createElement("a");
   para.className="button items";
@@ -407,7 +314,7 @@ function createItems(item, index, arr) {
   para.id="itemElemId";
 
   var myItem = false;
-  if(Me.myItemsList.indexOf(item) > -1){
+  if(Me.myItemList.indexOf(item) > -1){
     para.style.color = "ORANGE";
     myItem = true;
     item = item + " (my item)";
@@ -420,41 +327,46 @@ function createItems(item, index, arr) {
 };
 
 function displayCurrentGroup() {
-  var para = document.createElement("p");
-  para.className = "columnTitle";
-  var node = document.createTextNode("Village East 1");
-  para.appendChild(node);
-  var getDiv = document.getElementById("groupTitle");
-  getDiv.appendChild(para);
+  if(!titleItem.hasChildNodes()){  
+    groupNameTitle = document.createElement("p");
+    groupNameTitle.className = "columnTitle";
+    var node = document.createTextNode("Village East 1");
+    groupNameTitle.appendChild(node);
+    var getDiv = document.getElementById("groupTitle");
+    getDiv.appendChild(groupNameTitle);
+  }
 }
-
 
 function joinToggle(checkbox) {
     //toggle on
     if (!checkbox.checked){
+        var groupName = titleItem.childNodes[0].innerHTML;
+        Me.myGroupList.push(groupName);
 
         for(var i = memList.length - 1; 0 <= i; i--){
           if(memList[i] && memList[i].parentElement)
             memList[i].parentElement.removeChild(memList[i]);
         }
 
-        membersArray.push(Me.fullName);
-        membersArray.forEach(createMembers);
+        VilEast1.memberList.push(Me.fullName);
+        VilEast1.memberList.forEach(createMembers);
 
         for(var i = itemFetchList.length - 1; 0 <= i; i--){
           if(itemFetchList[i] && itemFetchList[i].parentElement)
             itemFetchList[i].parentElement.removeChild(itemFetchList[i]);
         }
 
-        for(var i = 0; i < Me.myItemsList.length; i++){
-          var myItemToAdd = Me.myItemsList[i];
-          itemListArray.push(myItemToAdd);
+        for(var i = 0; i < Me.myItemList.length; i++){
+          var myItemToAdd = Me.myItemList[i];
+          VilEast1.groupItemList.push(myItemToAdd);
         }
-        itemListArray.forEach(createItems);
+        VilEast1.groupItemList.forEach(createItems);
 
     }
     //toggle off
     else{
+      var groupName = titleItem.childNodes[0].innerHTML;
+      Me.myGroupList.splice(Me.myGroupList.indexOf(groupName), 1)
       //clear the memList
         for(var i = memList.length - 1; 0 <= i; i--){
           if(memList[i] && memList[i].parentElement)
@@ -462,8 +374,8 @@ function joinToggle(checkbox) {
         }
 
         //cut off the my name from the meemberArray
-        membersArray.splice(membersArray.indexOf(Me.fullName), 1);
-        membersArray.forEach(createMembers);
+        VilEast1.memberList.splice(VilEast1.memberList.indexOf(Me.fullName), 1);
+        VilEast1.memberList.forEach(createMembers);
 
 
         //clear all thes item from the displayList
@@ -472,10 +384,10 @@ function joinToggle(checkbox) {
             itemFetchList[i].parentElement.removeChild(itemFetchList[i]);
         }
         //remove my items from itemListArray
-        for(var i = 0; i < Me.myItemsList.length; i++){
-          itemListArray.splice(itemListArray.indexOf(Me.myItemsList[i]), 1);
+        for(var i = 0; i < Me.myItemList.length; i++){
+          VilEast1.groupItemList.splice(VilEast1.groupItemList.indexOf(Me.myItemList[i]), 1);
         }
-        itemListArray.forEach(createItems);
+        VilEast1.groupItemList.forEach(createItems);
     }
 }
 
